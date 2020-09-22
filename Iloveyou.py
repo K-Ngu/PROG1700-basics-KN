3511f4af99a063fccf7ab6a9bf28b7ed63edd437
Lily <kynn5862@gmail.com>
2020-09-22 10:58:48 -0300

    initial version

---
+++ Iloveyou.py
@@ -0,0 +1,20 @@
+print("Iloveyou")
+
+import tornado.ioloop
+import tornado.web
+import asyncio
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("I love you!")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
